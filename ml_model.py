9986a7a8de44fddc848e925a78f48a3ce90faff3
Sanket Kumar <smartsanket@gmail.com>
2021-06-07 18:39:08 -0400

    adding files

--- ml_model.py
+++ ml_model.py
@@ -3,7 +3,7 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import StandardScaler,OneHotEncoder
 from sklearn.model_selection import train_test_split
-import numpy as np
+#import numpy as np
 
 def predict(list_price,bedroom,washroom,additional,kitchens,style,housetype,fam,contractdate,garage):
 
